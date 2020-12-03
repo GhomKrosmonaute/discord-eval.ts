a80eb84e85b2c5d1185cdfae7d38b4af0f0c79b5
CamilleAbella <camille.abella@hotmail.fr>
2020-12-03 19:24:59 +0100

    added muted param

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 
-# DiscordEval.js
+# discord-eval.ts
 
 ## Install
 
@@ -9,20 +9,38 @@ npm install discord-eval.ts
 
 ## Example
 
-Here is the minimum required for your Eval command to work. **Think of securing access because a malicious Eval can be devastating for your PC !**
+Here is the minimum required for your Eval command to work. **Think of securing access because a malicious Eval can be devastating for your PC!**
 
 ```ts
-import discordEval from 'discord-eval.js'
-import Discord from 'discord.js'
+import discordEval from 'discord-eval.ts'
+import discord from 'discord.js'
 
 const client = new Discord.Client()
 
 client.login("token")
 
 client.on('message', async function(message: Discord.Message){
-	if(message.content.startsWith("js")){
-		let code = message.content.replace("js","").trim()
-		await discordEval(code, message)
-	}
+    // for "!run" command
+    if(message.content.startsWith("!run")){
+        // isolate code
+        const code = message.content.replace("!run","").trim()
+      
+        // has muted arg ?
+        const muted = code.includes("@muted")
+      
+        // eval the code
+        await discordEval(code, message, muted)
+    }
 })
 ```
+
+And use the command.
+
+```js
+!run
+// @muted
+const sum = 3 + 4
+return sum
+```
+
+Enjoy!
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,60 @@
 import Discord from "discord.js"
 
-const prettify = require("ghom-prettify");
-const codeInBlock = /^```(?:js)?\s(.+[^\\])```$/is;
+const prettify = require("ghom-prettify")
+const codeInBlock = /^```(?:js)?\s(.+[^\\])```$/is
 
-export default async function (code: string, message: Discord.Message): Promise<void> {
-  if (!code || message.system || message.author.bot) return;
+export default async function (
+  code: string,
+  message: Discord.Message,
+  muted: boolean = false
+): Promise<void> {
+  if (!code || message.system || message.author.bot) return
 
   /**
    * @type {?module:"discord.js".Message}
    */
-  let editable = null;
+  let editable = null
 
-  code = code.trim();
+  code = code.trim()
   if (codeInBlock.test(code)) {
-    code = code.replace(codeInBlock, "$1");
+    code = code.replace(codeInBlock, "$1")
   }
 
   if (code.includes("resolve") && !code.includes("new Promise")) {
-    code = `return await new Promise(resolve => {${code})`;
+    code = `return await new Promise(resolve => {${code})`
   }
 
   if (code.includes("await")) {
-    code = `async () => {${code}}`;
+    code = `async () => {${code}}`
 
-    let embed = new Discord.MessageEmbed()
-      .setTitle("DiscordEval")
-      .setDescription(`Running...`);
+    if (!muted) {
+      let embed = new Discord.MessageEmbed()
+        .setTitle("DiscordEval")
+        .setDescription(`Running...`)
 
-    editable = await message.channel.send(embed);
+      editable = await message.channel.send(embed)
+    }
   } else {
-    code = `() => {${code}}`;
+    code = `() => {${code}}`
   }
 
-  let out = null;
+  let out = null
   try {
-    out = await eval(code)();
+    out = await eval(code)()
   } catch (err) {
-    out = err;
+    out = err
   }
 
-  let classe = "void";
+  if (muted) return
+
+  let classe = "void"
   if (out !== undefined && out !== null) {
-    classe = out.constructor.name;
+    classe = out.constructor.name
   }
 
-  let formatted = code;
+  let formatted = code
   try {
-    formatted = await prettify(code, "js");
+    formatted = await prettify(code, "js")
   } catch (err) {}
 
   let embed = new Discord.MessageEmbed()
@@ -58,21 +66,21 @@ export default async function (code: string, message: Discord.Message): Promise<
       `Code ↓`,
       `\`\`\`js\n${formatted.length > 0 ? formatted : "void"}`.slice(0, 800) +
         `\n\`\`\``
-    );
+    )
 
   if (editable) {
-    await editable.edit(embed);
+    await editable.edit(embed)
   } else {
-    await message.channel.send(embed);
+    await message.channel.send(embed)
   }
 
   if (code.includes("return") || `${out}`.includes("Error")) {
-    embed = new Discord.MessageEmbed();
-    embed.setTitle(`Return ↓`);
+    embed = new Discord.MessageEmbed()
+    embed.setTitle(`Return ↓`)
     embed.setDescription(
       `\`\`\`js\n${`${out}`.length > 0 ? `${out}` : "void"}`.slice(0, 1800) +
         `\n\`\`\``
-    );
-    await message.channel.send(embed);
+    )
+    await message.channel.send(embed)
   }
-};
+}
